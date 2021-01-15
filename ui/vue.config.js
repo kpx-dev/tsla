5101c55602a11f99711b385ab31ea6ebfa0b4b70
Kien Pham <kien@kienpham.com>
2021-01-14 20:26:00 -0800

    z

--- ui/vue.config.js
+++ ui/vue.config.js
@@ -2,13 +2,4 @@ const path = require("path");
 
 module.exports = {
   outputDir: path.resolve(__dirname, "../docs"),
-  // publicPath: '/tsla/'
-  chainWebpack: config => {
-    config
-      .plugin('html')
-      .tap(args => {
-          args[0].title = "TSLA";
-          return args;
-      })
-  }
 }
